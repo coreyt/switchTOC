115208226920ce9c509391cbe1b63dfff7228c52
Corey Thompson <coreyt@gmail.com>
2023-01-19 17:05:30 -0600

    works; more resilient

--- switchTOC.lua
+++ switchTOC.lua
@@ -3,6 +3,33 @@ switchTOC – Switch from Azure Wiki TOC format to docx.
 Copyright © 2023 Corey Thompson
 MIT License
 ]]
+
+--[[
+This is a JSON representation of the part of the Pandoc AST that we wanted to catch.  A little tricky because the underline character == Emphasis in markdown.
+{
+"t": "Para",
+"c": [
+    {
+        "t": "Str",
+        "c": "[["
+    },
+    {
+        "t": "Emph",
+        "c": [
+            {
+                "t": "Str",
+                "c": "TOC"
+            }
+        ]
+    },
+    {
+        "t": "Str",
+        "c": "[["  <-- this is backwards
+    }
+]
+},
+]]
+
 local stringify_orig = (require 'pandoc.utils').stringify
 
 local function stringify(x)
@@ -12,19 +39,22 @@ end
 --- configs – these are populated in the Meta filter.
 local toc = {
   ooxml = [[
-    <w:sdt><w:sdtPr><w:docPartObj>
-    <w:docPartGallery w:val="Table of Contents" /><w:docPartUnique />
-    </w:docPartObj></w:sdtPr><w:sdtContent><w:p><w:pPr>
-    <w:pStyle w:val="TOCHeading" /></w:pPr><w:r>
-    <w:t>Table of Contents</w:t></w:r></w:p><w:p><w:r>
-    <w:fldChar w:fldCharType="begin" w:dirty="true" />
-    <w:instrText xml:space="preserve"> TOC \\o "1-2" \\h \\z \\u 
-    </w:instrText><w:fldChar w:fldCharType="separate" />
-    <w:fldChar w:fldCharType="end" /></w:r></w:p></w:sdtContent></w:sdt>
-  ]]
+<w:sdt>
+  <w:sdtContent>
+      <w:p>
+          <w:r>
+              <w:fldChar w:fldCharType="begin" w:dirty="true" />
+              <w:instrText xml:space="preserve">TOC \o "1-2" \h \z \u</w:instrText>
+              <w:fldChar w:fldCharType="separate" />
+              <w:fldChar w:fldCharType="end" />
+          </w:r>
+      </w:p>
+  </w:sdtContent>
+</w:sdt>
+]]
 }
 
---- Return a block element causing a page break in the given format.
+--- Return a block element that is appropriate for the given output format (we could make this work for different types of output.)
 local function newtoc(format)
   if format == 'docx' then
     return pandoc.RawBlock('openxml', toc.ooxml)
@@ -34,23 +64,16 @@ local function newtoc(format)
   end
 end
 
--- check that the block only contains '[[_TOC_]]'
-local function found_newtoc_command(command)
-  return command:match '^\[\[_TOC_\]\]%{?%}?$'
+
+-- [trace] Parsed [Para [Str "[[",Emph [Str "TOC"],Str "]]"]]
+function Para(elem)
+    if elem.content[1] and elem.content[1].text == '[[' and 
+    elem.content[2] and elem.content[2].tag == 'Emph' and 
+    elem.content[3] and elem.content[3].text == ']]' then
+        if elem.content[2].content[1].text =="TOC" then
+            return pandoc.RawBlock('openxml',toc.ooxml)
+        end
+    end
 end
 
--- Filter function called on each RawBlock element.
-function RawBlock (el)
-  -- check that the block is markdown.
-  if el.format:match 'markdown' and found_newtoc_command(el.text) then
-    -- use format-specific (docx) TOC code. FORMAT is set by pandoc to
-    -- the targeted output format.
-    return newtoc(FORMAT)
-  end
-  -- otherwise, leave the block unchanged
-  return nil
-end
-  
-  return {
-    {RawBlock = RawBlock, Para = Para}
-  }
+return {{ Para = Para }}
